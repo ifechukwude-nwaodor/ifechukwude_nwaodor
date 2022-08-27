453c444dd9577329995a0a4c1f553e66f60f29f2
ifechukwude_nwaodor <nwaodorifechukwude@gmail.com>
2022-08-27 12:36:56 +0100

    created landing page for my portfolio

---
+++ app.js
@@ -0,0 +1,14 @@
+let home = document.querySelector(".home")
+let blog = document.querySelector(".blog")
+let projects = document.querySelector(".projects")
+let contact = document.querySelector(".contact")
+let links = document.querySelectorAll(".nav-item")
+
+
+  AOS.init();
+
+
+
+
+
+
