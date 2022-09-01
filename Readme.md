7dbe1c6a7aee7870cf44f6e45a4613029e199b50
ifechukwude_nwaodor <nwaodorifechukwude@gmail.com>
2022-09-01 13:47:50 +0100

    worked on mobile responsiveness

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
-#My portfolio
-###This is my portfolio i created to showcase my works as a data analyst. It is going to host all my data projects.
+# My portfolio
+
+### This is my portfolio i created to showcase my works as a data analyst. It is going to host all my data projects.
--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-let home = document.querySelector(".home")
-let blog = document.querySelector(".blog")
-let projects = document.querySelector(".projects")
-let contact = document.querySelector(".contact")
-let links = document.querySelectorAll(".nav-item")
+let home = document.querySelector(".home");
+let blog = document.querySelector(".blog");
+let projects = document.querySelector(".project");
+let contact = document.querySelector(".contact");
+let linksCont = document.querySelectorAll(".nav-item")
+let links = document.querySelectorAll(".nav-link");
 
 
-  AOS.init();
+links.forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        const selectedlink = e.currentTarget.classList;
+        if (selectedlink.contains('home')){
+            links.classList.remove('active')
+            home.classList.add('active')
+            //linksCont.forEach((item)=>{
+                //item.removeActiveItem();
+                //item.classList.remove('active')
+                //home.classList.add('active')
+                
+            //})
+
+        } else if (selectedlink.contains('project')){
+            links.classList.remove('active')
+            projects.classList.add('active')
+                
+            
+        }
+    });
+});
 
 
 
